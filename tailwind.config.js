54ec2247e910377e31988635cf8cf0612f4d17c0
belapferreira <isabelapenhaferreira@gmail.com>
2023-09-18 14:51:43 -0300

    feat: set lint config & add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+
+      maxWidth: {
+        app: '76rem',
+      },
     },
   },
   plugins: [],
-}
+};
